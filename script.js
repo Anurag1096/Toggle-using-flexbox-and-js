38be579ded2e7c50b0ffa9aaff29cb029cafd5f2
Anurag <anurag.011996@gmail.com>
2021-03-16 17:30:42 +0530

    working

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const tog = document.querySelector(".topNav-toggle");
 
 tog.addEventListener("click", function () {
   const nav = document.querySelectorAll(".topNav__items");
-  nav.forEach((nav) => nav.classList.toggle(".topNav-toggleShow"));
+  nav.forEach((nav) => nav.classList.toggle("topNav-toggleShow"));
   console.log(nav);
 });
